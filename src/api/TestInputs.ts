a2553b79062f662f35996b73c25afcc2a7c6f568
Reid Moffat <18rem8@queensu.ca>
2025-07-29 20:46:56 -0400

    Document funcs

--- src/api/TestInputs.ts
+++ src/api/TestInputs.ts
@@ -4,19 +4,54 @@ class TestInputs {
 
     private readonly generators = new InputRegistry();
 
-    public getSimpleInputs = () => {
+    /**
+     * Gets all simple inputs
+     *
+     * This includes:
+     * -null & undefined
+     * -boolean
+     * -numbers (simple, like -1 and 1)
+     * -
+     *
+     * @returns An array of various simple values
+     */
+    public getSimpleInput() {
         return this.generators.getSimpleInputs();
     }
 
-    public getDetailedInputs = () => {
+    /**
+     * Gets all detailed inputs
+     *
+     * This includes:
+     * -
+     *
+     * @returns An array of various detailed values
+     */
+    public getDetailedInputs() {
         return this.generators.getDetailedInputs();
     }
 
-    public getLargeInputs = () => {
+    /**
+     * Gets large inputs to test limits
+     *
+     * This includes:
+     * -
+     *
+     * @returns An array of various simple values
+     */
+    public getLargeInputs() {
         return this.generators.getLargeInputs();
     }
 
-    public getAllInputs = () => {
+    /**
+     * Gets all inputs types
+     *
+     * This includes:
+     * -
+     *
+     * @returns An array of all values in this package
+     */
+    public getAllInputs() {
         return this.generators.getAllInputs();
     }
 }
