d9c21df4c9612ee9cd09117fed2daa14be05df2b
Reid Moffat <18rem8@queensu.ca>
2025-07-30 19:38:15 -0400

    Subcategory definitions

--- src/types/categories.ts
+++ src/types/categories.ts
@@ -9,8 +9,14 @@ type StringSubcategory =
     "special-chars" | "escape-sequences" | "json" | "html" | "paths" | "sql" |
     "regex" | "encoding" | "formatting" | "numbers-as-strings" | "booleans-as-strings" |
     "large" | "repeated" | "memory-intensive";
+type ArraySubcategory =
+    "";
+type ObjectSubcategory =
+    "";
+type OtherSubcategory =
+    "";
 
-type Subcategory = NumberSubcategory | StringSubcategory;
+type Subcategory = NumberSubcategory | StringSubcategory | ArraySubcategory | ObjectSubcategory | OtherSubcategory;
 
 export type { Category, Level };
-export type { Subcategory, NumberSubcategory, StringSubcategory };
+export type { Subcategory, NumberSubcategory, StringSubcategory, ArraySubcategory, ObjectSubcategory, OtherSubcategory };
