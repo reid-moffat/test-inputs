aa9b8753000f13acb79d117d824a975b4d8e5c1a
Reid Moffat <18rem8@queensu.ca>
2025-07-30 19:39:42 -0400

    Connected genertor types

--- src/types/InputGenerator.ts
+++ src/types/InputGenerator.ts
@@ -1,4 +1,13 @@
-import { Category, Level, NumberSubcategory, StringSubcategory, Subcategory } from "./categories";
+import {
+    Category,
+    Level,
+    Subcategory,
+    NumberSubcategory,
+    StringSubcategory,
+    ArraySubcategory,
+    ObjectSubcategory,
+    OtherSubcategory
+} from "./categories";
 
 type ValueWithDescription = {
     value: any;
@@ -24,18 +33,18 @@ type StringInputGenerator = InputGenerator & {
 
 type ArrayInputGenerator = InputGenerator & {
     category: "arrays";
-    subcategory: StringSubcategory;
+    subcategory: ArraySubcategory;
 }
 
 type ObjectInputGenerator = InputGenerator & {
     category: "objects";
-    subcategory: StringSubcategory;
+    subcategory: ObjectSubcategory;
 }
 
 type OtherInputGenerator = InputGenerator & {
     category: "other";
-    subcategory: StringSubcategory;
+    subcategory: OtherSubcategory;
 }
 
 export type { InputGenerator, ValueWithDescription };
-export type { NumberInputGenerator, StringInputGenerator };
+export type { NumberInputGenerator, StringInputGenerator, ArrayInputGenerator, ObjectInputGenerator, OtherInputGenerator };
