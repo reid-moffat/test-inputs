5e0b36a418c79cf4e2dbbd2a53a527d7400ade15
Reid Moffat <18rem8@queensu.ca>
2025-07-29 20:13:58 -0400

    Added index for all generators

---
+++ src/generators/index.ts
@@ -0,0 +1,6 @@
+import stringGenerators from "./strings";
+import { InputGenerator } from "../core/types";
+
+const allGenerators: InputGenerator[][] = [stringGenerators];
+
+export default allGenerators;
