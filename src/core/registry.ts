920a89b05f0ec041fe4fb06703617e4e286304e6
Reid Moffat <18rem8@queensu.ca>
2025-07-29 20:15:38 -0400

    REgister all gneerators

--- src/core/registry.ts
+++ src/core/registry.ts
@@ -1,10 +1,19 @@
 import { Categories, InputGenerator } from "./types";
+import allGenerators from "../generators/index";
 
 class InputRegistry {
 
-    private readonly generators = new Map<string, InputGenerator[]>();
+    private readonly generators: Map<string, InputGenerator[]> = new Map<string, InputGenerator[]>();
 
-    public register(generator: InputGenerator): void {
+    public constructor() {
+        allGenerators.forEach((generator: InputGenerator[]) => {
+            generator.forEach((generator: InputGenerator) => {
+                this.register(generator);
+            });
+        });
+    }
+
+    private register(generator: InputGenerator): void {
         const category: Categories = generator.category;
         if (!this.generators.has(category)) {
             this.generators.set(category, []);
