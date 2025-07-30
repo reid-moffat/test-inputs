84940d0731cb1a31c898457eecab654273374721
Reid Moffat <18rem8@queensu.ca>
2025-07-29 20:36:50 -0400

    API methods

--- src/api/TestInputs.ts
+++ src/api/TestInputs.ts
@@ -5,19 +5,19 @@ class TestInputs {
     private readonly generators = new InputRegistry();
 
     public getSimpleInputs = () => {
-        ;
+        return this.generators.getSimpleInputs();
     }
 
     public getDetailedInputs = () => {
-        ;
+        return this.generators.getDetailedInputs();
     }
 
     public getLargeInputs = () => {
-        ;
+        return this.generators.getLargeInputs();
     }
 
     public getAllInputs = () => {
-        ;
+        return this.generators.getAllInputs();
     }
 }
 
