c824b338846b62211ce84685c87e41bbc2be0cd8
Reid Moffat <18rem8@queensu.ca>
2025-07-31 15:05:31 -0400

    Spli tests

---
+++ test/rawInputs.test.ts
@@ -0,0 +1,10 @@
+import TestInputs from "test-inputs";
+
+suite("Suite name", function() {
+
+    test("Test name", function() {
+        const result = TestInputs.getInputs();
+
+        console.log(result);
+    });
+});
