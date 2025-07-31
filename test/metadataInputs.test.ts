0d035259734aedf39dbd0ccc0d3c881d48bab86f
Reid Moffat <18rem8@queensu.ca>
2025-07-31 16:05:17 -0400

    Added validator

--- test/metadataInputs.test.ts
+++ test/metadataInputs.test.ts
@@ -1,10 +1,26 @@
 import TestInputs, { InputItem } from "test-inputs";
+import { assert } from "chai";
 
 suite("Inputs with metadata", function() {
 
-    test("Test name", function() {
+    const validateInputItems = (items: InputItem[]) => {
+        for (let i = 0; i < items.length; ++i) {
+            const item = items[i];
+
+            assert.isObject(item);
+            assert.hasAllKeys(item, ["value", "description", "category", "subcategory", "level"]);
+
+            assert.isString(item.description);
+            assert.isString(item.category);
+            assert.isString(item.subcategory);
+            assert.isString(item.level);
+        }
+    }
+
+    test("Default call", function() {
         const result: InputItem[] = TestInputs.getInputs();
 
+        validateInputItems(result);
         console.log(result);
     });
 });
