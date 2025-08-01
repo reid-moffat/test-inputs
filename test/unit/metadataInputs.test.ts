5c56b026fab9a568db6228f94e439e4bbf2a5f57
Reid Moffat <18rem8@queensu.ca>
2025-08-01 13:19:43 -0400

    Split into sub-suites and added remaining cases

--- test/unit/metadataInputs.test.ts
+++ test/unit/metadataInputs.test.ts
@@ -3,55 +3,71 @@ import { validateEqualMetadata, validateInputItems } from "../validators.ts";
 
 suite("Inputs with metadata", function() {
 
-    test("Default call", function() {
-        const result: InputItem[] = TestInputs.getInputs();
-
-        validateInputItems(result);
-    });
-
-    test("Simple === default", function () {
-        const defaultResult: InputItem[] = TestInputs.getInputs();
-        const simpleResult: InputItem[] = TestInputs.getInputs({ include: { levels: 'simple' } });
-
-        validateEqualMetadata(defaultResult, simpleResult);
-    });
-
-    test("Detailed data", function () {
-        const result: InputItem[] = TestInputs.getInputs({ include: { levels: 'detailed' } });
-
-        validateInputItems(result);
-    });
-
-    test("Simple + detailed", function () {
-        const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['simple', 'detailed'] } });
-
-        validateInputItems(result);
-    });
-
-    test("Large data", function () {
-        const result: InputItem[] = TestInputs.getInputs({ include: { levels: 'large' } });
-
-        validateInputItems(result);
-    });
-
-    test("Simple + large", function () {
-        const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['simple', 'large'] } });
-
-        validateInputItems(result);
-    });
-
-    test("Detailed + large", function () {
-        const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['detailed', 'large'] } });
-
-        validateInputItems(result);
-    });
-
-    test("", function () {
+    suite("Invalid cases", function() {
 
     });
 
+    suite("Valid cases", function() {
 
-    test("", function () {
+        suite("By level", function() {
 
+            test("Simple data", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: 'simple' } });
+
+                validateInputItems(result);
+            });
+
+            test("Detailed data", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: 'detailed' } });
+
+                validateInputItems(result);
+            });
+
+            test("Large data", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: 'large' } });
+
+                validateInputItems(result);
+            });
+
+            test("Simple + detailed", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['simple', 'detailed'] } });
+
+                validateInputItems(result);
+            });
+
+            test("Simple + large", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['simple', 'large'] } });
+
+                validateInputItems(result);
+            });
+
+            test("Detailed + large", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['detailed', 'large'] } });
+
+                validateInputItems(result);
+            });
+
+            test("All levels", function () {
+                const result: InputItem[] = TestInputs.getInputs({ include: { levels: ['simple', 'detailed', 'large'] } });
+
+                validateInputItems(result);
+            });
+        });
+
+        suite("Specific checks", function() {
+
+            test("Default call", function() {
+                const result: InputItem[] = TestInputs.getInputs();
+
+                validateInputItems(result);
+            });
+
+            test("Simple === default", function () {
+                const defaultResult: InputItem[] = TestInputs.getInputs();
+                const simpleResult: InputItem[] = TestInputs.getInputs({ include: { levels: 'simple' } });
+
+                validateEqualMetadata(defaultResult, simpleResult);
+            });
+        });
     });
 });
