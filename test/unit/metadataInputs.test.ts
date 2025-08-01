990d610b523691c1f5b6fd2c9733697a096e5027
Reid Moffat <18rem8@queensu.ca>
2025-08-01 11:18:50 -0400

    Metadata test improv

--- test/unit/metadataInputs.test.ts
+++ test/unit/metadataInputs.test.ts
@@ -1,26 +1,13 @@
 import TestInputs, { InputItem } from "test-inputs";
+import { validateInputItems } from "../validators.ts";
 import { assert } from "chai";
 
 suite("Inputs with metadata", function() {
-
-    const validateInputItems = (items: InputItem[]) => {
-        for (let i: number = 0; i < items.length; ++i) {
-            const item: InputItem = items[i];
-
-            assert.isObject(item);
-            assert.hasAllKeys(item, ["value", "description", "category", "subcategory", "level"]);
-
-            assert.isString(item.description);
-            assert.isString(item.category);
-            assert.isString(item.subcategory);
-            assert.isString(item.level);
-        }
-    }
 
     test("Default call", function() {
         const result: InputItem[] = TestInputs.getInputs();
 
         validateInputItems(result);
-        console.log(result);
+        assert.equal(result.length, 156);
     });
 });
--- test/unit/toJson.test.ts
+++ test/unit/toJson.test.ts
@@ -1,9 +1,10 @@
 import TestInputs from "test-inputs";
+import { InputItem } from "../../src";
 
 suite("Inputs to JSON", function() {
 
     test("Simple call", function() {
-        const result = TestInputs.getInputs();
+        const result: InputItem[] = TestInputs.getInputs();
         console.log(result);
     });
 });
