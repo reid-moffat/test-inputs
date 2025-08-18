99a2fa3ba81a053dbad0c0d8dfd765d651737520
Reid Moffat <18rem8@queensu.ca>
2025-08-18 18:53:17 -0400

    toJSON test

--- test/unit/toJson.test.ts
+++ test/unit/toJson.test.ts
@@ -1,10 +1,8 @@
 import TestInputs from "test-inputs";
-import { InputItem } from "../../src";
 
 suite("Inputs to JSON", function() {
 
     test("Simple call", function() {
-        const result: InputItem[] = TestInputs.getInputs();
-        console.log(result);
+        const result: string = TestInputs.toJSON();
     });
 });
