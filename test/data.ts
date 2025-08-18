90d89774dc091095f04d5bee445f603f107c88f8
Reid Moffat <18rem8@queensu.ca>
2025-08-18 14:27:09 -0400

    Added more invalid test cases

--- test/data.ts
+++ test/data.ts
@@ -6,11 +6,74 @@ type InvalidOption = {
     message: string;
 }
 
+const excludeIncludeMessage = (filters: string[]) => {
+    return `Cannot define both 'exclude' and 'include' for the same filter type (${filters.join(', ')}). `
+        + `Using 'include' makes all non-specified filters excluded, and vice-verse for 'exclude'`;
+}
+
 const invalidOptions: InvalidOption[] = [
+    {
+        name: "Include & exclude simple level (both strings)",
+        input: { include: { levels: 'simple' }, exclude: { levels: 'simple' } },
+        message: excludeIncludeMessage(['levels'])
+    },
+    {
+        name: "Include & exclude simple level (array & string)",
+        input: { include: { levels: ['simple'] }, exclude: { levels: 'simple' } },
+        message: excludeIncludeMessage(['levels'])
+    },
+    {
+        name: "Include & exclude simple level (string & array)",
+        input: { include: { levels: 'simple' }, exclude: { levels: ['simple'] } },
+        message: excludeIncludeMessage(['levels'])
+    },
+    {
+        name: "Include & exclude simple level (both arrays)",
+        input: { include: { levels: ['simple'] }, exclude: { levels: ['simple'] } },
+        message: excludeIncludeMessage(['levels'])
+    },
+    {
+        name: "Include & exclude all levels",
+        input: { include: { levels: ['simple', 'large', 'detailed'] }, exclude: { levels: ['simple', 'large', 'detailed'] } },
+        message: excludeIncludeMessage(['levels'])
+    },
+    {
+        name: "Include all levels, exclude detailed",
+        input: { include: { levels: ['simple', 'large', 'detailed'] }, exclude: { levels: 'detailed' } },
+        message: excludeIncludeMessage(['levels'])
+    },
+
+    {
+        name: "Include & exclude array category (both strings)",
+        input: { include: { categories: 'arrays' }, exclude: { categories: 'arrays' } },
+        message: excludeIncludeMessage(['categories'])
+    },
+    {
+        name: "Include & exclude array category (array and string)",
+        input: { include: { categories: ['arrays'] }, exclude: { categories: 'arrays' } },
+        message: excludeIncludeMessage(['categories'])
+    },
+    {
+        name: "Include & exclude array category (string and array)",
+        input: { include: { categories: 'arrays' }, exclude: { categories: ['arrays'] } },
+        message: excludeIncludeMessage(['categories'])
+    },
+    {
+        name: "Include & exclude array category (both arrays)",
+        input: { include: { categories: ['arrays'] }, exclude: { categories: ['arrays'] } },
+        message: excludeIncludeMessage(['categories'])
+    },
+
     {
         name: "Include & exclude simple level",
         input: { include: { levels: 'simple' }, exclude: { levels: 'simple' } },
-        message: "Cannot define both exclude and include for any filter (levels)"
+        message: excludeIncludeMessage(['levels'])
+    },
+
+    {
+        name: "Include & exclude simple level",
+        input: { include: { levels: 'simple' }, exclude: { levels: 'simple' } },
+        message: excludeIncludeMessage(['levels'])
     }
 ];
 
