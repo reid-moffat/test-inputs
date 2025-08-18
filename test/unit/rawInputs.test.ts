b8cb72c0cc510aa2b452d28f7caf2d81c0de6e66
Reid Moffat <18rem8@queensu.ca>
2025-08-18 18:49:21 -0400

    Added invalid cases to raw inputs

--- test/unit/rawInputs.test.ts
+++ test/unit/rawInputs.test.ts
@@ -1,6 +1,18 @@
-import TestInputs from "test-inputs";
+import TestInputs, { InputItem } from "test-inputs";
+import { invalidOptions } from "../data.ts";
+import { assert } from "chai";
 
 suite("Raw inputs", function() {
+
+    suite("Invalid cases", function() {
+
+        invalidOptions.map((option) =>
+            test(option.name, function () {
+                const func: () => InputItem[] = (): InputItem[] => TestInputs.getRawInputs(option.input);
+                assert.throws((): InputItem[] => func(), Error, option.message);
+            })
+        );
+    });
 
     test("Test name", function() {
         const result: any[] = TestInputs.getRawInputs();
