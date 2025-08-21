3ea52f47bc782ee6bdf46abc4d006ec1470b01ec
Reid Moffat <18rem8@queensu.ca>
2025-08-21 18:38:38 -0400

    Document I/O types

--- src/types/io.ts
+++ src/types/io.ts
@@ -1,11 +1,18 @@
 import { Category, Level, Subcategory } from "./filters.ts";
 
+/**
+ * Filters to pass to an input query in order to include or exclude certain types
+ *
+ * All filters are optional
+ */
 type FilterOptions = {
+    /** Values to explicitly include. If a type (e.g. levels) is defined, any levels not provided will be excluded */
     include?: {
         levels?: Level | Level[];
         categories?: Category | Category[];
         subcategories?: Subcategory | Subcategory[];
     };
+    /** Values to explicitly exclude. If a type (e.g. levels) is defined, any levels not provided will be included */
     exclude?: {
         levels?: Level | Level[];
         categories?: Category | Category[];
@@ -13,12 +20,20 @@ type FilterOptions = {
     };
 }
 
+/**
+ * A generated input item with its metadata
+ */
 type InputItem = {
+    /** The input value */
     value: any;
+    /** The literal code used to create this value */
     description: string;
-    category: Category;
-    subcategory: Subcategory;
+    /** Complexity level this value belongs to (simple, detailed, large) */
     level: Level;
+    /** Category this value belongs to (numbers, arrays, etc) */
+    category: Category;
+    /** Sub-category of the given category this value belongs to (boundaries, scientific, etc) */
+    subcategory: Subcategory;
 }
 
 export type { FilterOptions, InputItem };
