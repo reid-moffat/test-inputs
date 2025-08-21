e3da9587d34c36ba1380e303d1570cef9684612a
Reid Moffat <18rem8@queensu.ca>
2025-08-21 18:21:28 -0400

    Export values from package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import TestInputs from "./core/TestInputs";
+import {
+    CategoryValues,
+    LevelValues,
+    SubcategoryValues,
+    NumberSubcategoryValues,
+    StringSubcategoryValues,
+    ArraySubcategoryValues,
+    ObjectSubcategoryValues,
+    OtherSubcategoryValues
+} from "./types/filters.ts";
 import type {
     Category,
     Level,
@@ -11,6 +21,9 @@ import type {
 } from "./types/filters.ts";
 import type { FilterOptions, InputItem } from "./types/io";
 
+
 export default TestInputs;
+
+export { CategoryValues, LevelValues, SubcategoryValues, NumberSubcategoryValues, StringSubcategoryValues, ArraySubcategoryValues, ObjectSubcategoryValues, OtherSubcategoryValues };
 export type { Category, Level, Subcategory, NumberSubcategory, StringSubcategory, ArraySubcategory, ObjectSubcategory, OtherSubcategory };
 export type { FilterOptions, InputItem };
