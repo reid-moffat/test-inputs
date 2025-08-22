17c9382cc039704ed21f99275877e9d139ccb0e4
Reid Moffat <18rem8@queensu.ca>
2025-08-22 14:24:55 -0400

    Test for toJSON

--- test/unit/toJson.test.ts
+++ test/unit/toJson.test.ts
@@ -2,8 +2,21 @@ import TestInputs from "test-inputs";
 
 suite("Inputs to JSON", function() {
 
-    test("Simple call", function() {
-        // TODO
-        //const result: string = TestInputs.toJSON();
+    test("Print details", function() {
+        const result: Record<string, any> = TestInputs.toJSON();
+
+        for (const category of Object.keys(result)) {
+            console.log(`===Category: ${category}===`);
+
+            for (const subcategory of Object.keys(result[category])) {
+                console.log(`Subcategory: ${subcategory}`);
+                console.log(`Level: ${result[category][subcategory].level}`);
+                console.log(`Values: ${result[category][subcategory].values.length}`);
+
+                console.log();
+            }
+
+            console.log();
+        }
     });
 });
