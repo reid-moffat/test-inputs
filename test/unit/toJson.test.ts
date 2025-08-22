47b485ebe8485c20b198f16f652f5cc0a4fddf63
Reid Moffat <18rem8@queensu.ca>
2025-08-22 15:03:48 -0400

    Added default call + validator

--- test/unit/toJson.test.ts
+++ test/unit/toJson.test.ts
@@ -1,22 +1,64 @@
-import TestInputs from "test-inputs";
+import TestInputs, { CategoryValues } from "test-inputs";
+import { assert } from "chai";
 
 suite("Inputs to JSON", function() {
 
-    test("Print details", function() {
-        const result: Record<string, any> = TestInputs.toJSON();
+    /**
+     * Validates a result has the correct structure
+     */
+    function validateStructure(record: any) {
+        assert.isObject(record);
 
-        for (const category of Object.keys(result)) {
+        const categories: string[] = Object.keys(record);
+        assert.includeMembers([...CategoryValues], categories, `Some object category key(s) are invalid. `
+            + `Expected ${JSON.stringify([...CategoryValues])}, got ${JSON.stringify(categories)}`);
+
+        for (const category of categories) {
+
+        }
+    }
+
+    test("All JSON printed", function() {
+        const record: Record<string, any> = TestInputs.toJSON({});
+        validateStructure(record);
+
+        for (const category of Object.keys(record)) {
             console.log(`===Category: ${category}===`);
 
-            for (const subcategory of Object.keys(result[category])) {
+            for (const subcategory of Object.keys(record[category])) {
                 console.log(`Subcategory: ${subcategory}`);
-                console.log(`Level: ${result[category][subcategory].level}`);
-                console.log(`Values: ${result[category][subcategory].values.length}`);
+                console.log(`Level: ${record[category][subcategory].level}`);
+                console.log(`Values: ${record[category][subcategory].values.length}`);
 
                 console.log();
             }
 
             console.log();
+        }
+    });
+
+    test("Default call", function () {
+        const record: Record<string, any> = TestInputs.toJSON();
+        validateStructure(record);
+
+        for (const category of Object.keys(record)) {
+            for (const subcategory of Object.keys(record[category])) {
+                const val = record[category][subcategory];
+
+                assert.include(["simple", "detailed"], val.level);
+            }
+        }
+    });
+
+
+    test("", function () {
+        const record: Record<string, any> = TestInputs.toJSON();
+        assert.isObject(record);
+
+        for (const category of Object.keys(record)) {
+            for (const subcategory of Object.keys(record[category])) {
+                ;
+            }
         }
     });
 });
